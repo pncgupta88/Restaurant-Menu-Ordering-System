47fc04ffb723f59ebbb5d571c30fad88b82fc6db
pncgupta88 <princelj11e@gmail.com>
2025-06-18 18:08:30 +0530

    Initial commit

---
+++ restaurantMenuOderingSystem.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+class Self_Order{
+private:
+    int Order_No = 0;
+    string eating_location;
+    int product_Id;
+    
+
+public:
+    Self_Order() 
+    {
+        cout << "WELCOME TO \"SLAYER'S DEN\" YOUR ALL IN ONE RESTAURANT" << endl;
+
+        cout << "SELECT EATING LOCATION" << endl << "(a)IN" << endl << "(b)OUT" << endl << "ENTER 'IN' OR 'OUT': ";
+        cin >> eating_location;
+
+        while (eating_location != "IN" && eating_location != "OUT")
+        {
+            cout << "INVALID INPUT! ENTER EATING LOCATION AGAIN: ";
+            cin >> eating_location;
+        }
+
+
+        
+    }
+};
+
+int main(){
+    
+    Self_Order order1;
+    
+
+    return 0;
+}
