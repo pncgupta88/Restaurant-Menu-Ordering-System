e72f8914cd73dea6c89e40d70c70ca69d3103428
pncgupta88 <princelj11e@gmail.com>
2025-06-21 21:26:59 +0530

    Make code much better and efficient than previous.

--- restaurantMenuOderingSystem.cpp
+++ restaurantMenuOderingSystem.cpp
@@ -1,203 +1,374 @@
-#include <iostream>
-#include <algorithm>
-#include <string>
-#include <fstream>
-#include <vector>
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<vector>
 
 using namespace std;
 
-class Self_Order
+struct usable_menu
 {
-public:
-    string eating_location;
-    double Total_Bill = 0;
+    int item_id;
+    string item_name;
+    double item_price;
+};
+struct save_order
+{
+    int save_id;
+    string save_name;
+    int save_qty;
+    double save_unitprice;
+    double save_itemtotal;
+};
 
-    Self_Order()
-    {
-        cout << "WELCOME TO \"SLAYER'S DEN\" YOUR ALL IN ONE RESTAURANT" << endl;
+void add_newitem();
+void display_menu();
+void take_order();
+bool check_duplicate(const string &new_itemname);
+int auto_assign_new_itemID();
+vector<usable_menu> copymenuitems();
 
-        cout << "SELECT EATING LOCATION" << endl
-             << "(a)IN" << endl
-             << "(b)OUT" << endl
-             << "ENTER 'IN' OR 'OUT': ";
-        cin >> eating_location;
+class RMOS
+{
+    private:
+    int first_choice;
+    int insidefirst_choice;
 
-        while (eating_location != "IN" && eating_location != "OUT")
-        {
-            cout << "INVALID sINPUT! ENTER EATING LOCATION AGAIN: ";
-            cin >> eating_location;
-        }
+    public:
+
+    RMOS(){
         cout << endl;
-        cout << "EATING LOCATION SUCESSFULLY SAVED!" << endl;
-    }
+        cout << "|| WELCOME TO RESTAURANT MENU ORDERING SYSTEM ||"<< endl << endl;
+        cout << "CHOOSE AN OPTION:" << endl << "1) EDIT MENU." << endl << "2) VIEW MENU AND PLACE ORDER." << endl << "3) Exit"<< endl << endl;
+        cout << "ENTER 1/2/3: ";
 
-    void displayMenu()
-    {
-        ifstream menuFile;
-        menuFile.open("newMenu.txt"); 
-        if (!menuFile.is_open())
+        while (!(cin >> first_choice) || (first_choice != 1 && first_choice != 2 && first_choice != 3))
         {
-            cout << "Error! could not open menu file." << endl;
-            return;
+            cout << "INVALID INPUT! ENTER 1/2/3: ";
+            cin.clear();
+            cin.ignore(10000, '\n');
         }
 
-        cout << "\n=============MENU=============\n\n";
-        cout << "Id.  item Name       Price(Rs)" << endl << endl;
-
-        string line;
-        
-        while (getline(menuFile, line))
+        if (first_choice == 1)
         {
-            cout << line << endl;
-        }
+            cout << endl << "SELECT WHAT YOU WANT TO EDIT IN MENU:"<< endl;
+            cout << "1) ADD AN ITEM." << endl;
+            cout << "2) DELETE AN ITEM.(INCOMPLETE! NOT WORKING)" << endl;
+            cout << "3) EDIT AN ITEM.(INCOMPLETE! NOT WORKING)" << endl << endl;
+            cout << "ENTER 1/2/3: ";
 
-        cout << "\n---------------------------\n\n";
-        menuFile.close();
-    }
-
-    struct Menu_Items
-    {
-        int item_ID;        
-        string item_Name;
-        double item_Price;
-    };
-
-    struct saveOrder
-    {
-        string name;
-        int quantity;
-        double unit_Price;
-        double item_Total;
-    };
-    
-
-    /*This funcn makes copy of item details from menu.txt file
-    and stores it in vector menuItems so they can be used to 
-    take orders.*/ 
-    vector<Menu_Items> copyMenuItems()
-    {
-        vector<Menu_Items> menuItems;
-        ifstream menuFile;
-        menuFile.open("newMenu.txt");
-        if(!menuFile.is_open()){
-            cout << "Error! could not open menu file." << endl;
-            return menuItems;
-        }
-
-        Menu_Items temp_Item;
-        while (menuFile >> temp_Item.item_ID >> temp_Item.item_Name >> temp_Item.item_Price)
-        {
-            menuItems.push_back(temp_Item);
-        }
-        menuFile.close();
-        return menuItems;        
-    }
-    
-    /*This funcn takes order of customer:
-    1) Customer enters item id then the system checks the item id in menu that it is valid or not.
-    2) If id is not valid then system asks customer item id again.
-    3) If id is valid then system asks for item quantity.
-    4) Then it calculate prices and saves item details in vector Ordered_Item for further uses.
-    5) After getting details system asks user to order more items or not.*/
-    void take_Order()
-    {
-        vector<Menu_Items> menuItems = copyMenuItems();
-        vector<saveOrder> Ordered_Item;
-
-        char add_More = 'y';
-
-        while (add_More == 'y' || add_More == 'Y')
-        {
-            cout << "Enter the item ID you want to order: ";
-            int entered_ID;
-            cin >> entered_ID;
-
-            bool valid_item = false;
-            Menu_Items selected_item;
-
-            for (int i = 0; i < menuItems.size(); i++)
+            while (!(cin >> insidefirst_choice) || (insidefirst_choice != 1)) //&& insidefirst_choice != 2 && insidefirst_choice != 3))
             {
-                if (menuItems[i].item_ID == entered_ID)
+                cout << "INVALID INPUT! ENTER 1/2/3: ";
+                cin.clear();
+                cin.ignore(10000, '\n');
+            }
+            
+            if (insidefirst_choice == 1)
+            {
+                char addmore = 'Y';
+                
+                while (addmore == 'Y' || addmore == 'y')
                 {
-                    selected_item = menuItems[i];
-                    valid_item = true;
-                    break;
-                }                
-            }
+                    cout << endl << "ENTER NEW ITEM DETAILS"<<endl <<endl;
+                    add_newitem();
+                    cout << endl << "SUCESSFULLY ADDED NEW ITEM IN MENU."<< endl << endl;
 
-            if (!valid_item)
-            {
-                cout << "Invalid item ID. Please enter a valid item ID."<<endl;
-                continue;
-            }
+                    cout << "DO YOU WANT TO ADD MORE ITEMS. ENTER Y/y FOR 'YES' OR N/n FOR 'NO': ";
+                    while (!(cin >> addmore) || (addmore != 'y' && addmore != 'Y' && addmore != 'n' && addmore != 'N'))
+                    {
+                        cout << "INVALID INPUT! ENTER Y/y FOR 'YES' OR N/n FOR 'NO': ";
+                        cin.clear();
+                        cin.ignore(10000 , '\n');
+                    }
+                    
+                    if (addmore == 'N' || addmore == 'n')
+                    {
+                        cout << endl <<"THANK YOU! FOR USING OUR SYSTEM. VISIT AGAIN BYII..." << endl<<endl;
+                
+                    }
+                    
+                }
+             
+            }          
+        }else if (first_choice == 2)
+        {
+            display_menu();
+            // string eating_location;
+            // cout << "ENTER EATING LOCATION. ENTER 'IN' FOR EAT IN OR 'OUT' FOR EAT OUT: ";
+            // while (!(cin >> eating_location) || (eating_location != "IN" && eating_location != "in" && eating_location != "OUT" && eating_location != "out"))
+            // {
+            //     cout << "INVALID! EATING LOCATION.ENTER 'IN' FOR EAT IN OR 'OUT' FOR EAT OUT: ";
+            //     cin.clear();
+            //     cin.ignore(10000, '\n');
+            // }
             
-            int qty;
-            cout << "Enter " << selected_item.item_Name<<" quantity: ";
-            cin >> qty;
-            cout << endl;
-            
-            double subtotal = selected_item.item_Price * qty;
-            Total_Bill += subtotal;
+            cout << endl << "PLACE YOUR ORDER HERE: "<< endl << endl;
+            take_order();
 
-            saveOrder order;
-            order.name = selected_item.item_Name;
-            order.quantity = qty;
-            order.unit_Price = selected_item.item_Price;
-            order.item_Total = subtotal;
-            Ordered_Item.push_back(order);
-
-            cout << "Add more items? (y/n): ";
-            cin >> add_More;
-            cout << endl;
-            
+            // cout << "  Eating Location: "<< eating_location<<endl;
+            // cout << "  Thank you! Visit again..."<< endl<<endl;
+            // cout << "============================="<<endl <<endl;            
+        }else if(first_choice == 3)
+        {
+            cout << endl <<"EXITING..."<<endl;
+            exit(0);
         }
-
-        /*After taking order system asks customer to place or cancel the order for confermation
-        1) If customer places order then system shows bill to customer and restarts again for next customer.
-        2) If customer cancel order the system restares again for next customer.*/
-
-        string confirmation;
-        cout << "Enter 'P/p' to place order or 'C/c' to cancel the order: ";
-        cin >> confirmation;
-        cout << endl;
-
-        if (confirmation == "P" || confirmation == "p")
-        {
-            cout << "Order Placed Sucessfully. Please wait a while your order is preparing..."<< endl <<endl;
-            cout << "============Bill============"<<endl<<endl;
-            cout << "        SLAYER'S DEN        "<<endl;
-            cout << "|YOUR ALL IN ONE RESTAURANT|"<<endl<<endl;
-            for (int i = 0; i < Ordered_Item.size(); i++)
-            {
-                cout << i+1<<")" <<"Item Name: " << Ordered_Item[i].name << endl;
-                cout << "  Item Quantity: "<< Ordered_Item[i].quantity << endl;
-                cout << "  Item Unit Price: "<< Ordered_Item[i].unit_Price << endl;
-                cout << "  Item Total Price: "<< Ordered_Item[i].item_Total << endl << endl;
-            }
-
-            cout << "  Total bill: "<< Total_Bill << endl;
-            cout << "  Eating Location: "<< eating_location<<endl;
-            cout << "  Thank you! Visit again..."<< endl<<endl;
-            cout << "============================="<<endl <<endl;            
-        }else
-        {
-            cout << "Order Cancelled."<<endl << endl;
-        }   
-        
     }
 };
 
-int main()
+int auto_assign_new_itemID()
 {
+    ifstream menu("MENU.txt");
+    int id;
+    int max_id = 0;
+    string name;
+    double price;
+
+    while (menu >> id >> name >> price)
+    {
+        if(id > max_id)
+        {
+            max_id = id;
+        }
+    }
+
+    int new_id = max_id + 1;
+    return new_id;    
+}
+bool check_duplicate(const string &new_itemname)
+{
+    ifstream menu("MENU.txt");
+
+    int id;
+    string name;
+    double price;
+
+    while (menu >> id >> name >> price)
+    {
+        if (name == new_itemname)
+        {
+            return true;
+        }        
+    }
+    return false;
+}
+void add_newitem()
+{
+    ofstream add("MENU.txt" , ios::app);
+
+    int new_itemid = auto_assign_new_itemID();
+    string new_itemname;
+    double new_itemprice;
+
+    cout << "SYSTEM WILL ASSIGN ID TO NEW ITEM AUTOMATICALLY "<< endl; 
+
     while (true)
     {
-        Self_Order orders;
-        orders.displayMenu();
-        orders.take_Order();
+        cout << "ENTER NEW ITEM'S NAME: ";
+        cin >> new_itemname;
 
-        cout << "Restarting the system for next customer."<< endl;
-        cout << "Restarting..."<< endl << endl;
+        if(check_duplicate(new_itemname))
+        {
+            cout << endl << "ITEM ALREADY EXISTS IN MENU!" << endl << endl;
+        }else
+        {
+            break;
+        }
+    }   
+
+    cout << "ENTER NEW ITEM'S PRICE: ";
+    cin >> new_itemprice; 
+    add << new_itemid << "   " << new_itemname << "        " << new_itemprice << endl;
+}
+
+// void delete_item()
+// {
+
+// }
+
+void display_menu()
+{
+    ifstream item_menu;
+    item_menu.open("MENU.txt");
+
+    cout << "\n=============MENU=============\n\n";
+    cout << "Id.  item Name       Price(Rs)" << endl << endl;
+
+    string line;
+
+    while (getline(item_menu, line))
+    {
+        cout << line << endl;
+    }
+
+    cout << "\n---------------------------\n\n";
+    
+    item_menu.close();
+
+}
+// struct usable_menu
+// {
+//     int item_id;
+//     string item_name;
+//     double item_price;
+// };
+// struct save_order
+// {
+//     int save_id;
+//     string save_name;
+//     int save_qty;
+//     double save_unitprice;
+//     double save_itemtotal;
+// };
+vector<usable_menu> copymenuitems()
+{
+    vector<usable_menu> menutotakeorder;
+    ifstream menu("MENU.txt");
+
+    usable_menu temp;
+    while (menu >> temp.item_id >> temp.item_name >> temp.item_price )
+    {
+        menutotakeorder.push_back(temp);
+    }
+    menu.close();
+    return menutotakeorder;
+}
+void take_order()
+{
+    vector<usable_menu> menu = copymenuitems();
+    vector<save_order> order_details;
+
+    char order_more = 'Y';
+    double total = 0;
+
+    string eating_location;
+    cout << "ENTER EATING LOCATION. ENTER 'IN' FOR EAT IN OR 'OUT' FOR EAT OUT: ";
+    while (!(cin >> eating_location) || (eating_location != "IN" && eating_location != "in" && eating_location != "OUT" && eating_location != "out"))
+        {
+            cout << "INVALID! EATING LOCATION.ENTER 'IN' FOR EAT IN OR 'OUT' FOR EAT OUT: ";
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
+
+    while (order_more == 'Y' || order_more == 'y')
+    {
+        cout << "ENTER ITEM ID: ";
+        int entered_id;
+        while (!(cin >> entered_id))
+        {
+            cout << "INVALID INPUT! ENTER ITEM ID FROM MENU: ";
+            cin.clear();
+            cin.ignore(10000 , '\n');
+        }
+        
+        bool valid_orderid = false;
+        usable_menu selected_item;
+
+        for (int i = 0; i < menu.size(); i++)
+        {
+            if (menu[i].item_id == entered_id)
+            {
+                selected_item = menu[i];
+                valid_orderid = true;
+                break;
+            }
+            
+        }
+        if(!valid_orderid)
+        {
+            cout << "INVALID ITEM ID! ENTER VALID ITEM ID FROM MENU: ";
+            continue;
+        }
+
+        int qty;
+        cout << "ENTER QUANTITY OF " << selected_item.item_name << " : ";
+        while (!(cin >> qty) || qty < 0)
+        {
+            cout << "INVALID INPUT! ENTER QUANTITY OF " << selected_item.item_name<< " : ";
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
+
+        double subtotal = selected_item.item_price * qty;
+        total += subtotal;
+
+        save_order order;
+        order.save_id = selected_item.item_id;
+        order.save_name = selected_item.item_name;
+        order.save_qty = qty;
+        order.save_unitprice = selected_item.item_price;
+        order.save_itemtotal = subtotal;
+        order_details.push_back(order);
+
+        cout << endl << "DO YOU WANT TO ADD MORE ITEMS. ENTER Y/y FOR 'YES' OR N/n FOR 'NO': ";
+        while (!(cin >> order_more) || (order_more != 'Y' && order_more != 'y' && order_more != 'N' && order_more != 'n'))
+        {
+            cout << "INVALID INPUT! ENTER Y/y FOR 'YES' OR N/n FOR 'NO': ";
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
+        
+        cout << endl;
+
+        if (order_more == 'N' || order_more == 'n')
+        {
+            cout << "ITEMS IS/ARE SUCESSFULLY ADDED. ENTER P/p TO PLACE ORDER OR C/c TO CANCEL THE ORDER: ";
+
+            char final_confirmation;
+            while (!(cin >> final_confirmation) || (final_confirmation != 'P' && final_confirmation != 'p' && final_confirmation != 'C' && final_confirmation != 'c'))
+            {
+                cout << "INVALID INPUT! ENTER P/p TO PLACE ORDER OR C/c TO CANCEL THE ORDER: ";
+                cin.clear();
+                cin.ignore(10000, '\n');
+            }
+            
+            if (final_confirmation == 'P' || final_confirmation == 'p')
+            {
+                cout << endl << "YOUR ORDER IS SUCESSFULLY PLACED! PLEASE WAIT A MOMENT, YOUR ORDER IS BEING PREPARED..."<< endl << endl;
+
+                cout << "============Bill============"<<endl<<endl;
+                for (int i = 0; i < order_details.size(); i++)
+                {
+                    cout << i+1 << ")ITEM ID: " << order_details[i].save_id<<endl;
+                    cout << "  ITEM NAME: " << order_details[i].save_name<<endl;
+                    cout << "  ITEM QUANTITY: " << order_details[i].save_qty<<endl;
+                    cout << "  ITEM UNIT PRICE: " << order_details[i].save_unitprice<<endl;
+                    cout << "  ITEM SUBTOTAL: " << order_details[i].save_itemtotal<<endl << endl;
+                }
+            
+                cout << "  TOTAL AMOUNT: " << total << endl;
+                cout << "  EATING LOCATION: "<< eating_location<<endl;
+
+                ofstream billout("ORDER.txt");
+
+                for (int i = 0; i < order_details.size(); i++)
+                {
+                    billout << order_details[i].save_id <<"  "<< order_details[i].save_name << "  "<< order_details[i].save_qty << "  "<< order_details[i].save_unitprice << "  "<< order_details[i].save_itemtotal << endl;
+                }
+                
+                billout.close();
+
+                cout << endl << "  BILL SUSCESSFULLY SAVED IN ORDER.txt FILE."<<endl;
+                cout << "  THANK YOU! VISIT AGAIN..."<< endl<<endl;
+                cout << "============================="<<endl <<endl;
+
+            }else
+            {
+                cout << endl << "YOUR ORDER HAS BEEN CANCELLED. THANK YOU FOR VISITING!" << endl;
+            }
+        } 
     }    
+}
 
+
+int main(){
+
+    while (true)
+    {
+        RMOS new_order;
+        cout << "RESTARTING...SYSTEM"<< endl;
+    }
+    
+    
     return 0;
 }
